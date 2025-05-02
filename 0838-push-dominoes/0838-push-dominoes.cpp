5dd70d47003dba6381c291e810f2a987885ebe95
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-05-02 09:44:15 +0530

    Time: 17 ms (54.64%), Space: 20.4 MB (60.71%) - LeetHub

---
+++ 0838-push-dominoes/0838-push-dominoes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector<int> forces(n, 0);
+        
+        
+        int force = 0;
+        for (int i = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') {
+                force = n;          
+            } else if (dominoes[i] == 'L') {
+                force = 0;            
+            } else {
+                force = max(force - 1, 0);
+            }
+            forces[i] += force;
+        }
+        
+
+        force = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            if (dominoes[i] == 'L') {
+                force = n;          
+            } else if (dominoes[i] == 'R') {
+                force = 0;           
+            } else {
+                force = max(force - 1, 0);
+            }
+            forces[i] -= force;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (forces[i] > 0)      dominoes[i] = 'R';
+            else if (forces[i] < 0) dominoes[i] = 'L';
+            else                    dominoes[i] = '.';
+        }
+        
+        return dominoes;
+    }
+};
