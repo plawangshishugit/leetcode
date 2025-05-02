543e653079b17c28d375c1e9c4cfac3000cce56f
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-05-02 09:46:22 +0530

    Time: 15 ms (62.86%), Space: 20.4 MB (60.71%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.cpp
+++ 0838-push-dominoes/0838-push-dominoes.cpp
@@ -2,55 +2,39 @@ class Solution {
 public:
     string pushDominoes(string dominoes) {
         int n = dominoes.size();
-        vector<int> R_force(n, INT_MAX);
-        vector<int> L_force(n, INT_MAX);
-     
-        int current_force = 0;
+        vector<int> forces(n, 0);
+        
+        int force = 0;
         for (int i = 0; i < n; ++i) {
             if (dominoes[i] == 'R') {
-                current_force = 1;
-                R_force[i] = 0;
+                force = n;            
             } else if (dominoes[i] == 'L') {
-                current_force = 0;
+                force = 0;            
             } else {
-                if (current_force > 0) {
-                    R_force[i] = current_force;
-                    current_force++;
-                }
+                force = max(force - 1, 0);
             }
+            forces[i] += force;
         }
-        
-     
-        current_force = 0;
+ 
+        force = 0;
         for (int i = n - 1; i >= 0; --i) {
             if (dominoes[i] == 'L') {
-                current_force = 1;
-                L_force[i] = 0;
+                force = n;            
             } else if (dominoes[i] == 'R') {
-                current_force = 0;
+                force = 0;           
             } else {
-                if (current_force > 0) {
-                    L_force[i] = current_force;
-                    current_force++;
-                }
+                force = max(force - 1, 0);
             }
+            forces[i] -= force;
         }
         
-   
-        string result;
+      
         for (int i = 0; i < n; ++i) {
-            if (dominoes[i] != '.') {
-                result.push_back(dominoes[i]);
-                continue;
-            }
-            if (R_force[i] < L_force[i]) {
-                result.push_back('R');
-            } else if (L_force[i] < R_force[i]) {
-                result.push_back('L');
-            } else {
-                result.push_back('.');
-            }
+            if (forces[i] > 0)      dominoes[i] = 'R';
+            else if (forces[i] < 0) dominoes[i] = 'L';
+            else                    dominoes[i] = '.';
         }
-        return result;
+        
+        return dominoes;
     }
 };
