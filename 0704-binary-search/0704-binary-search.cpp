8a991e75e6713abc90fc1703f489211c2ca7f542
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-08-08 06:28:45 +0530

    Time: 0 ms (100%), Space: 31.5 MB (9.52%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,24 +1,28 @@
-class Solution{
+class Solution {
 public:
-    int search(vector<int> &nums, int target)
+    int bs(vector<int>&nums,int s, int e, int target)
     {
-        return bs(nums, 0 , nums.size()-1, target);
-    }
-private:
-    int bs(vector<int> &nums, int s, int e, int target){
-        while(s <= e)
-        {
-            int mid = s + (e-s)/2;
+        if(s>e)return -1;
 
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(target < nums[mid]){
-                return bs(nums, s , mid -1, target);
-            }else{
-                return bs(nums, mid+1, e , target);
-            }
+        int mid = s + (e-s)/2;
+        if(nums[mid] == target)
+        {
+            return mid;
         }
-        return -1;
+        if(target > nums[ mid]){
+            return bs(nums,mid+1, e, target);
+        }else{
+            return bs(nums,s,mid-1,target);
+        }
+
+    }   
+
+    int search(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        return bs(nums,s,e,target);
+        
     }
 };
