8086c78d1c0e744ed6aa43b3e83ba9166e072e50
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-09-26 07:25:03 +0530

    Time: 3 ms (66.55%), Space: 8 MB (19.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,16 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int fib(int n) {
-        static vector<int> dp(31, -1);
-        if (n <= 1) return n;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = fib(n-1) + fib(n-2);
+        if (n <= 1) return n;               // handle n==0 and n==1 safely
+        vector<int> ans(n+1, 0);
+        ans[0] = 0;
+        ans[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
     }
 };
