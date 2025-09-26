cce6cf909935724aa6ef158e664cc9c664dc55e7
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-09-26 07:22:21 +0530

    Time: 3 ms (66.55%), Space: 7.9 MB (36.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        static vector<int> dp(31, -1);  // LeetCode constraint: n ≤ 30
+        static vector<int> dp(31, -1);
         if (n <= 1) return n;
         if (dp[n] != -1) return dp[n];
         return dp[n] = fib(n-1) + fib(n-2);
