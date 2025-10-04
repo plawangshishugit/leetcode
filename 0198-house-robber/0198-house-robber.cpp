7189fcc66ac18fe772e95bc84017d75e60dda0c6
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-10-04 23:28:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    int solve(int i, vector<int>& nums) {
+        // base cases
+        if (i < 0) return 0;
+        if (i == 0) return nums[0];
+
+        // choice: rob current or skip
+        int rob = nums[i] + solve(i-2, nums);
+        int skip = solve(i-1, nums);
+
+        return max(rob, skip);
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-        }
-
-        return dp[n-1];
+        return solve(n-1, nums);
     }
 };
