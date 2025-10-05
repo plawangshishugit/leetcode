f8e8de51c2988de64bb5d633c58943b11dd2e497
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2025-10-05 09:12:48 +0530

    Time: 3 ms (7.61%), Space: 10.9 MB (40.87%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    // helper function for linear house robber problem
-    int houseRobber1(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        
-        for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-        }
-        return dp[n-1];
+    int solve(int i, vector<int>& nums, vector<int>& dp) {
+        if (i < 0) return 0;
+        if (i == 0) return nums[0];
+
+        if (dp[i] != -1) return dp[i];
+
+        int rob = nums[i] + solve(i-2, nums, dp);
+        int skip = solve(i-1, nums, dp);
+
+        return dp[i] = max(rob, skip);
     }
-    
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        
-        vector<int> temp1, temp2;
-        
+        if (n == 1) return nums[0];  // special case
+
         // case 1: exclude first house
-        for(int i = 1; i < n; i++){
-            temp1.push_back(nums[i]);
-        }
-        
+        vector<int> temp1(nums.begin() + 1, nums.end());
+        vector<int> dp1(temp1.size(), -1);
+        int case1 = solve(temp1.size() - 1, temp1, dp1);
+
         // case 2: exclude last house
-        for(int i = 0; i < n-1; i++){
-            temp2.push_back(nums[i]);
-        }
-        
-        return max(houseRobber1(temp1), houseRobber1(temp2));
+        vector<int> temp2(nums.begin(), nums.end() - 1);
+        vector<int> dp2(temp2.size(), -1);
+        int case2 = solve(temp2.size() - 1, temp2, dp2);
+
+        return max(case1, case2);
     }
 };
