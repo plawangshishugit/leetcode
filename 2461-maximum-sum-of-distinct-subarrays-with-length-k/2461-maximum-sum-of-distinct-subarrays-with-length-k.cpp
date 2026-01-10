365b49a731b736ed1f6d0c3d0c3053fc9cebb8d8
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-01-10 11:54:58 +0530

    Time: 221 ms (9.23%), Space: 104.3 MB (7.12%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        map<int, int> freq;
+        long long sum = 0, maxSum = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+            freq[nums[right]]++;
+
+            // Maintain window size k
+            if (right - left + 1 > k) {
+                sum -= nums[left];
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+                left++;
+            }
+
+            // Check valid window
+            if (right - left + 1 == k && freq.size() == k) {
+                maxSum = max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+    }
+};
