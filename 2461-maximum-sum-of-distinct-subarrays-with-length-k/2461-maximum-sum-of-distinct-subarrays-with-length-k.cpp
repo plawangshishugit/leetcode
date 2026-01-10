0db04ba30508069898f60eb1f57a7adb67f370b7
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-01-10 12:12:32 +0530

    Time: 118 ms (25.23%), Space: 99.8 MB (24.85%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
+        unordered_set<int> st;
         long long sum = 0, maxSum = 0;
         int left = 0;
 
         for (int right = 0; right < nums.size(); right++) {
-            sum += nums[right];
-            freq[nums[right]]++;
 
-            // Shrink window if size exceeds k
-            if (right - left + 1 > k) {
+            // Remove duplicates
+            while (st.count(nums[right])) {
+                st.erase(nums[left]);
                 sum -= nums[left];
-                freq[nums[left]]--;
-
-                if (freq[nums[left]] == 0)
-                    freq.erase(nums[left]);
-
                 left++;
             }
 
-            // Valid window: size k & all elements distinct
-            if (right - left + 1 == k && freq.size() == k) {
+            st.insert(nums[right]);
+            sum += nums[right];
+
+            // Maintain window size k
+            if (st.size() > k) {
+                st.erase(nums[left]);
+                sum -= nums[left];
+                left++;
+            }
+
+            // Valid window
+            if (st.size() == k) {
                 maxSum = max(maxSum, sum);
             }
         }
