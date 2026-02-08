7a80fdfabbe8274e85908f2a2dfd1b528f690945
Plawang Shishu <153839161+plawangshishugit@users.noreply.github.com>
2026-02-08 18:12:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.67%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        //brute force
+        // //brute force
         int m = s.length();
         int n = goal.length ();
 
         //check if both length is equal or not?if not return false
         if( m != n) return false;
-        for(int cnt = 0; cnt <m; cnt++){
-            if(s == goal) return true;
-            rotate(s.begin(), s.begin()+1, s.end());
+        // for(int cnt = 0; cnt <m; cnt++){
+        //     if(s == goal) return true;
+        //     rotate(s.begin(), s.begin()+1, s.end());
+        // }
+        // return false;
+
+        //optimized solution
+        // observe that string s ko maximum s.length() baar rotate ke dene ke baad wahi milta hai to hm s mein se s ko hi concatinate kr de dekhte hain ki concatinated string mein s ke sare rotation mil jate hain to is problem mein hm concatinated string mein goal ko find() kr sakte hain if yes return true otherwise return false;
+        if((s+s).find(goal) != string :: npos){
+            return true;
         }
         return false;
-
     }
 };
 //rotate ka syntax
